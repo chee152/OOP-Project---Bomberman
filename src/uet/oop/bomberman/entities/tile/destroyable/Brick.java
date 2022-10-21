f6c038f2f9247cd3acef8c53a175968beec6a3af
hgqh <88188821+hgqh@users.noreply.github.com>
2022-10-22 00:14:37 +0700

    brick+ destroyableTile

--- src/uet/oop/bomberman/entities/tile/destroyable/Brick.java
+++ src/uet/oop/bomberman/entities/tile/destroyable/Brick.java
@@ -1,16 +1,18 @@
 package uet.oop.bomberman.entities.tile.destroyable;
+
 import javafx.scene.image.Image;
 import uet.oop.bomberman.Game;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Brick extends DestroyableTile {
-    public Brick(int x, int y, Image img)
-    {
-        super(x,y,img);
+
+
+    public void setExploded(boolean exploded) {
+        this.exploding = exploded;
     }
-    public void setExplode(boolean explode)
-    {
-        this.explode = explode;
+
+    public Brick(int x, int y, Image img) {
+        super(x, y, img);
     }
 
     @Override
@@ -20,37 +22,37 @@ public class Brick extends DestroyableTile {
         animate();
         setImg(sprite.getFxImage());
     }
-    public void handleBrick(){
-        if (!destroyed) {
-            timeToDisappear++;
-            if (timeToDisappear < Game.TIME_TO_DISAPPEAR) {
-                explode = true;
-            } else {
-                explode = false;
-                timeToDisappear = 0;
-                destroyed = true;
-            }
-        }
-    }
-    public void animate()
-    {
-        if (animate < 7500) animate++;
+
+
+
+    private void animate() {
+        if (animate < 1) animate++;
         else animate = 0;
     }
-    public void chooseSprite()
-    {
-        int time = Game.TIME_TO_DISAPPEAR;
-        if (explode)
-        {
+
+    private void chooseSprite() {
+        int _time = Game.TIME_TO_DISAPPEAR;
+        if (exploding) {
             sprite = Sprite.movingSprite(
                     Sprite.brick_exploded,
                     Sprite.brick_exploded1,
                     Sprite.brick_exploded2,
-                    animate,
-                    time);
-        }
-        else {
+                    animate, _time);
+        } else {
             sprite = Sprite.brick;
         }
+    }
+    private void handleBrick() {
+        if (!destroyed) {
+            timeToDisapear++;
+            if (timeToDisapear < Game.TIME_TO_DISAPPEAR) {
+                exploding = true;
+            } else {
+                exploding = false;
+                timeToDisapear = 0;
+                destroyed = true;
+            }
+        }
+
     }
 }
--- src/uet/oop/bomberman/entities/tile/destroyable/DestroyableTile.java
+++ src/uet/oop/bomberman/entities/tile/destroyable/DestroyableTile.java
@@ -1,19 +1,19 @@
 package uet.oop.bomberman.entities.tile.destroyable;
+
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Tile;
 
-public class DestroyableTile extends Tile{
+public class DestroyableTile extends Tile {
     protected int animate = 0;
-    protected boolean explode = false;
+    protected boolean exploding = false;
+    protected int timeToDisapear = 0;
     protected boolean destroyed = false;
-    protected int timeToDisappear = 0;
-
-    public DestroyableTile(int xUnit, int yUnit, Image img)
-    {
-        super(xUnit, yUnit, img);
-    }
 
     public boolean isDestroyed() {
         return destroyed;
+    }
+
+    public DestroyableTile(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
     }
 }
