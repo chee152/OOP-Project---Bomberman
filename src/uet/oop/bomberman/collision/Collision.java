fa777f21611d4467192e07898c3d9906ce009dcc
hgqh <88188821+hgqh@users.noreply.github.com>
2022-10-22 00:18:45 +0700

    Update Collision.java

--- src/uet/oop/bomberman/collision/Collision.java
+++ src/uet/oop/bomberman/collision/Collision.java
@@ -1,7 +1,7 @@
 package uet.oop.bomberman.collision;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.graphics.Sprite;
-public class collision {
+public class Collision {
     public static boolean CheckCollision(Entity A, Entity B) {
         double leftB, leftA;
         double rightA,rightB;
@@ -20,5 +20,24 @@ public class collision {
 
         return !((bottomA <= topB) || (topA >= bottomB) || (rightA <= leftB) || (leftA >= rightB));
     }
-    
+    public static boolean checkCollisionWithBuffer(Entity A, Entity B) {
+       int buff=10;
+        double leftB, leftA;
+        double rightA,rightB;
+        double topA, topB;
+        double bottomA, bottomB;
+
+
+        leftB = B.getX();
+        rightB = leftB + Sprite.SCALED_SIZE;
+        topB = B.getY();
+        bottomB = topB + Sprite.SCALED_SIZE;
+
+        leftA = A.getX() - buff;
+        rightA = A.getX() + Sprite.SCALED_SIZE + buff;
+        topA = A.getY() - buff;
+        bottomA = A.getY() + Sprite.SCALED_SIZE + buff;
+
+        return !((bottomA <= topB) || (topA >= bottomB) || (rightA <= leftB) || (leftA >= rightB));
+    }
 }
