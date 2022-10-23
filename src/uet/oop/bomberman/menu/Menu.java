3912fad09fa2a2eefe02074952330cd4c3118fee
hgqh <88188821+hgqh@users.noreply.github.com>
2022-10-23 21:12:46 +0700

    1 chút menu

---
+++ src/uet/oop/bomberman/menu/Menu.java
@@ -0,0 +1,67 @@
+package uet.oop.bomberman.menu;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.Side;
+import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import uet.oop.bomberman.graphics.Sprite;
+
+import static uet.oop.bomberman.Game.HEIGHT;
+import static uet.oop.bomberman.Game.WIDTH;
+
+public abstract class Menu {
+    protected abstract Scene create();
+
+    protected static BackgroundImage createImage(String url) {
+        return new BackgroundImage(
+                new Image(url),
+                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
+                new BackgroundPosition(Side.LEFT, 0, true, Side.BOTTOM, 0, true),
+//				new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, false, true));
+                new BackgroundSize(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT, true, true, false, true));
+
+    }
+
+    protected void customText(Text text) {
+        DropShadow ds = new DropShadow();
+        ds.setOffsetY(3.0f);
+        ds.setColor(Color.color(0.4f, 0.4f, 0.4f));
+
+        Font font = Font.loadFont("file:res/emulogic.ttf", 45);
+
+        text.setEffect(ds);
+        text.setFont(font);
+
+        text.setFill(new LinearGradient(0, 0, 1, 1, true,
+                CycleMethod.REFLECT,
+                new Stop(0.0, Color.RED),
+                new Stop(1.0, Color.GOLD)));
+        text.setStroke(Color.BLUEVIOLET);
+        text.setStrokeWidth(0.5);
+
+        DropShadow shadow = new DropShadow();
+        shadow.setOffsetY(5.0);
+        Reflection r = new Reflection();
+        r.setFraction(0.6f);
+        text.setEffect(r);
+    }
+
+    protected VBox initVBox() {
+        VBox vb = new VBox();
+        vb.setAlignment(Pos.BOTTOM_CENTER);
+        vb.setPadding(new Insets(20));
+        vb.setSpacing(30);
+        vb.setBackground(new Background(createImage("file:res/background.jpg")));
+        return vb;
+    }
+}
---
+++ src/uet/oop/bomberman/menu/ScoreInMenu.java
@@ -0,0 +1,89 @@
+package uet.oop.bomberman.menu;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.showScore.Score;
+
+import java.io.IOException;
+
+import static uet.oop.bomberman.Game.HEIGHT;
+import static uet.oop.bomberman.Game.WIDTH;
+
+public class ScoreInMenu extends Menu {
+    public static boolean SCORE_BACK = false;
+    private final Score score;
+    private String str = "0";
+    private VBox box;
+    private Text backText;
+    private Text scoreinf;
+    private TextFlow area;
+
+    public ScoreInMenu(Score score) throws IOException {
+        this.score = score;
+        create();
+    }
+
+    public void updateScore() {
+        str = "";
+        for (int i = 0; i < score.getHighScore().size(); i++) {
+            str += i + 1 + ". " + score.getHighScore().get(i) + "\n";
+        }
+        scoreinf.setText(str);
+    }
+
+
+    @Override
+    public Scene create() {
+        box = initVBox();
+        backText = new Text("Back");
+        area = new TextFlow();
+        scoreinf = new Text(str);
+
+        customText(backText);
+        customText(scoreinf);
+//		scoreInfo.setStyle("-fx-font-size:25");
+        scoreinf.setEffect(null);
+
+        area.getChildren().add(scoreinf);
+        box.getChildren().add(area);
+        box.getChildren().add(backText);
+        VBox.setMargin(area, new Insets(100, 200, 0, 300));
+        box.setFillWidth(true);
+
+        Scene scoreOption = new Scene(box, Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
+
+        EventHandler<MouseEvent> playHandle = new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                playHandle(event, backText);
+            }
+        };
+
+
+        backText.addEventFilter(MouseEvent.MOUSE_ENTERED, playHandle);
+        backText.addEventFilter(MouseEvent.MOUSE_EXITED_TARGET, playHandle);
+        backText.addEventFilter(MouseEvent.MOUSE_PRESSED, playHandle);
+
+        return scoreOption;
+    }
+
+
+    private static void playHandle(MouseEvent event, Text text) {
+        if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
+            text.setStyle("-fx-font-size:60");
+        } else if (event.getEventType() == MouseEvent.MOUSE_EXITED_TARGET) {
+            text.setStyle("-fx-font-size:45");
+        } else if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+            SCORE_BACK = true;
+            text.setStyle("-fx-font-size:45");
+        } else {
+            text.setStyle("-fx-font-size:45");
+        }
+    }
+
+}
