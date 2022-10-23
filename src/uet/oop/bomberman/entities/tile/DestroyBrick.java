6667e17b027d7a773e3775ca865be80ce6daed5b
hgqh <88188821+hgqh@users.noreply.github.com>
2022-10-23 13:38:09 +0700

    DestroyBrick

--- src/uet/oop/bomberman/entities/tile/DestroyBrick.java
+++ src/uet/oop/bomberman/entities/tile/DestroyBrick.java
@@ -1,19 +1,31 @@
-package uet.oop.bomberman.entities.tile.destroyable;
+package uet.oop.bomberman.entities.tile;
 
 import javafx.scene.image.Image;
 import uet.oop.bomberman.Game;
+import uet.oop.bomberman.entities.Tile;
 import uet.oop.bomberman.graphics.Sprite;
 
-public class Brick extends DestroyableTile {
 
+public class DestroyBrick extends Tile {
+    protected int animate = 0;
+    protected boolean exploding = false;
+    protected int timeToDisapear = 0;
+    protected boolean destroyed = false;
 
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public DestroyBrick(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
     public void setExploded(boolean exploded) {
         this.exploding = exploded;
     }
 
-    public Brick(int x, int y, Image img) {
-        super(x, y, img);
-    }
+   // public Brick(int x, int y, Image img) {
+      //  super(x, y, img);
+   // }
 
     @Override
     public void update() {
